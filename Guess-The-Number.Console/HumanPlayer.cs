7793c44d3984dceb50261ada3a4a6c5a6b95f45c
Addis <lumiaddis@gmail.com>
2024-05-21 23:49:00 -0400

    Agregue comentarios en mi clase HumanPlayer

--- Guess-The-Number.Console/HumanPlayer.cs
+++ Guess-The-Number.Console/HumanPlayer.cs
@@ -1,20 +1,20 @@
 using System;
-using System.Collections.Generic;
 
 namespace GuessTheNumber
 {
-     class HumanPlayer : Player
+     class HumanPlayer : Player //Clase HumanPlayer heredada de Player, con sus propiedades y métodos
     {
-        public HumanPlayer() : base( "Addis" )
+        public HumanPlayer() : base( "Addis" )  //Llama al constructor de la clase base Player con el nombre "Addis".
         {
         }
 
-        //Ocupo override para reescribir el metodo MakeGuess()
+        //Sobrescribe el método MakeGuess() definido en la clase base Player, con override.
         public override int MakeGuess()
         {   
-            Console.Write( "Ingresa tu número: " );
-            int Guesses = Convert.ToInt32(Console.ReadLine());
-            return Guesses;
+            //Este método solicita al jugador humano que ingrese un número.
+            Console.Write( "Ingresa tu número: " ); //Lee la entrada del usuario desde la consola, la convierte a un entero y la asigna a la variable Guesses.
+            int Guesses = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32 se usa para convertir la entrada de texto en un número entero.
+            return Guesses; //Devuelve el número ingresado por el jugador.
         }
     }
 }
